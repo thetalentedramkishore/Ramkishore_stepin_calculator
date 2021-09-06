95b71ba8cff4c093e0e517b66ded185c3d0f5dd0
thetalentedramkishore <89726572+thetalentedramkishore@users.noreply.github.com>
2021-09-06 13:18:34 +0530

    Delete .vscode/implementation directory

--- .vscode/implementation/addition.c
+++
@@ -1,8 +0,0 @@
-#include "calculator.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-error_t add(arthimetic_inputs* ai){
-    ai->output=(ai->input_1)+(ai->input_2);
-    return SUCCESS;
-}
